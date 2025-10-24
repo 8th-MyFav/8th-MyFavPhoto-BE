3c32b96d2893fa919bc7211b7963b8f7846ac21e
apple <apple@apples-MacBook-Air.local>
2025-10-24 18:22:50 +0900

    feat: get point api 구현

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,13 +1,24 @@
 import userService from "../services/userService.js";
 
-// NOTE: 내 정보 확인
+// NOTE: 내 정보 조회
 export async function getMe(req, res, next) {
   try {
     const { userId } = req.auth;
 
     const user = await userService.getMeInfo(userId);
-    console.log(user);
     return res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+}
+
+// NOTE: 내 point 조회
+export async function getPoints(req, res, next) {
+  try {
+    const { userId } = req.auth;
+
+    const point = await userService.getPointInfo(userId);
+    return res.status(200).json(point);
   } catch (error) {
     next(error);
   }
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,4 +1,5 @@
 import authRepository from "../repositories/authRepository.js";
+import pointRepository from "../repositories/pointRepository.js";
 
 // NOTE: 유저 정보 가져오기
 async function getMeInfo(id) {
@@ -11,8 +12,38 @@ async function getMeInfo(id) {
     }
 
     const { password, refreshToken, ...filteredUser } = user;
-    console.log(filteredUser);
     return filteredUser;
+  } catch (error) {
+    if (error.code === 401) {
+      throw error;
+    }
+
+    const customError = new Error("데이터베이스 작업 중 오류가 발생했습니다");
+    customError.code = 500;
+    throw customError;
+  }
+}
+
+// NOTE: point 가져오기
+async function getPointInfo(id) {
+  try {
+    const user = await authRepository.findById(id);
+    if (!user) {
+      const error = new Error("로그인이 필요합니다.");
+      error.code = 401;
+      throw error;
+    }
+
+    console.log("user=> ",user);
+
+    const points  = await pointRepository.findById(id);
+    if (!points.acc_point) {
+     points.acc_point = 0;
+    }
+
+    console.log("point=> ", points.acc_point);
+
+    return points.acc_point;
   } catch (error) {
     if (error.code === 401) {
       throw error;
@@ -26,4 +57,5 @@ async function getMeInfo(id) {
 
 export default {
   getMeInfo,
+  getPointInfo,
 };
