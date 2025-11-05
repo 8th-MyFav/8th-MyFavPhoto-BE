02ff1cea4f44f0e7c1df81c16500c56ec1de1911
MyeongjuHong <hmjpearl@gmail.com>
2025-11-05 17:50:27 +0900

    hotfix: listing 관련 에러 수정
    ### schema.prisma
    - TradePosts 테이블에 price 필드 추가
    
    ### listingController.js
    - cardId 파싱 수정
    
    ### listService.js
    - Photocards price 값 가져와 매핑한 뒤 createPost에 전달
    - update.coount -> update.count 오타 수정
    - tradPostId 가져와 NOT_FOUND 에러 처리
    - updatePhotocard -> updateTradePrice 함수명 변경
    - 정렬 조건 변경 🌟
    - 첫번째 카드 매핑

--- src/repositories/listingRepository.js
+++ src/repositories/listingRepository.js
@@ -24,12 +24,14 @@ async function createTradePost({
   trade_grade,
   trade_genre,
   trade_note,
+  price,
 }) {
   return tx.tradePosts.create({
     data: {
       trade_grade,
       trade_genre,
       trade_note,
+      price,
     },
   });
 }
@@ -78,10 +80,10 @@ async function setSaleStatus({ tx = prisma, ids }) {
   });
 }
 
-// NOTE: photocards 테이블 가격 수정 (genre, grade 고정)
-async function updatePhotocard({ tx = prisma, cardId, price }) {
-  return tx.photocards.update({
-    where: { id: cardId },
+// NOTE: tradePosts 테이블 가격 수정 (genre, grade 고정)
+async function updateTradePrice({ tx = prisma, tradePostId, price }) {
+  return tx.tradePosts.update({
+    where: { id: tradePostId },
     data: { price },
   });
 }
@@ -115,6 +117,16 @@ async function updateTradePost({
   });
 }
 
+// NOTE: trade post id로 잠금 설정
+async function findAndLockTradePostById({ tx = prisma, id }) {
+  return tx.tradePosts
+    .findUniqueOrThrow({
+      // 테이블에서 id 일치하는 당일 행 조회
+      where: { id },
+    })
+    .forUpdate(); // 행 수준 잠금 (다른 트랜잭션에서 수정/삭제 금지)
+}
+
 /* delete listing */
 
 async function findStatusTrue({ tx = prisma, cardId }) {
@@ -137,7 +149,7 @@ async function findAll({ where, take, cursor, orderBy }) {
     take, // 기준점 이후 개수 제한 반환
     cursor: cursor ? { id: cursor } : undefined, // 기준점 기준: id
     skip: cursor ? 1 : 0, // 기준점 있으면 제외, 없으면 포함
-    orderBy,
+    orderBy, // QUES: 이 orderBy가 findAll 함수 외부에서 동적으로 결정될 일이 뭐가 있지..?
     include: {
       // userPhotocards join
       UserPhotocards: {
@@ -170,10 +182,11 @@ export default {
   countByCardId,
   findUserPhotocardsByCardId,
   setSaleStatus,
-  updatePhotocard,
+  updateTradePrice,
   findTradePostIdByCardId,
   updateTradePost,
   findStatusTrue,
   deleteTradePost,
   findAll,
+  findAndLockTradePostById,
 };
